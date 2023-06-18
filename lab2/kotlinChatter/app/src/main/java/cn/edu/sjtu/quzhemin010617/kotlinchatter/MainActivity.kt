ce7c22365c8f0bbafc7900697b1d4b96c5d4e92b
Benj-Qu <quzhemin@umich.com>
2023-06-18 22:17:18 +0800

    Lab2 Front End

--- lab2/kotlinChatter/app/src/main/java/cn/edu/sjtu/quzhemin010617/kotlinchatter/MainActivity.kt
+++ lab2/kotlinChatter/app/src/main/java/cn/edu/sjtu/quzhemin010617/kotlinchatter/MainActivity.kt
@@ -5,6 +5,8 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableList
 import cn.edu.sjtu.quzhemin010617.kotlinchatter.ChattStore.chatts
 import cn.edu.sjtu.quzhemin010617.kotlinchatter.ChattStore.getChatts
 import cn.edu.sjtu.quzhemin010617.kotlinchatter.databinding.ActivityMainBinding
@@ -12,6 +14,24 @@ import cn.edu.sjtu.quzhemin010617.kotlinchatter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var view: ActivityMainBinding
     private lateinit var chattListAdapter: ChattListAdapter
+
+    private val propertyObserver = object: ObservableList.OnListChangedCallback<ObservableArrayList<Int>>() {
+        override fun onChanged(sender: ObservableArrayList<Int>?) { }
+        override fun onItemRangeChanged(sender: ObservableArrayList<Int>?, positionStart: Int, itemCount: Int) { }
+        override fun onItemRangeInserted(
+            sender: ObservableArrayList<Int>?,
+            positionStart: Int,
+            itemCount: Int
+        ) {
+            println("onItemRangeInserted: $positionStart, $itemCount")
+            runOnUiThread {
+                chattListAdapter.notifyDataSetChanged()
+            }
+        }
+        override fun onItemRangeMoved(sender: ObservableArrayList<Int>?, fromPosition: Int, toPosition: Int,
+                                      itemCount: Int) { }
+        override fun onItemRangeRemoved(sender: ObservableArrayList<Int>?, positionStart: Int, itemCount: Int) { }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,23 +43,26 @@ class MainActivity : AppCompatActivity() {
         view.chattListView.setAdapter(chattListAdapter)
         
         view.refreshContainer.setOnRefreshListener {
-            refreshTimeline()
+            getChatts()
         }
 
-        refreshTimeline()
+        getChatts()
+
+        chatts.addOnListChangedCallback(propertyObserver)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        chatts.removeOnListChangedCallback(propertyObserver)
     }
 
     fun startPost(view: View?) = startActivity(Intent(this, PostActivity::class.java))
 
     private fun refreshTimeline() {
-        getChatts(applicationContext) {
-            runOnUiThread {
-                // inform the list adapter that data set has changed
-                // so that it can redraw the screen.
-                chattListAdapter.notifyDataSetChanged()
-            }
-            // stop the refreshing animation upon completion:
-            view.refreshContainer.isRefreshing = false
-        }
+        getChatts()
+
+        // stop the refreshing animation upon completion:
+        view.refreshContainer.isRefreshing = false
     }
 }
